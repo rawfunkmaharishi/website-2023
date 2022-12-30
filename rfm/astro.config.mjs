698fff53439c1e45f9672ba81c0845ee6e5a5377
pikesley <sam.pikesley@gmail.com>
2022-12-30 12:19:50 +0000

    Tidy that HTML

--- rfm/astro.config.mjs
+++ rfm/astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
+import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rawfunkmaharishi.uk",
   vite: {
     plugins: [yaml()],
   },
+  integrations: [
+    htmlBeautifier({
+      indent_size: 2,
+      wrap_line_length: 88,
+      jslint_happy: true,
+      preserve_newlines: false,
+    }),
+  ],
 });

5521b8fca721c3a7a3e3eba8b7a46f60e0463f62
pikesley <sam.pikesley@gmail.com>
2023-01-03 21:25:27 +0000

    Remove `yaml` support

--- rfm/astro.config.mjs
+++ rfm/astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
-import yaml from "@rollup/plugin-yaml";
 import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rawfunkmaharishi.uk",
-  vite: {
-    plugins: [yaml()],
-  },
   integrations: [
     htmlBeautifier({
       indent_size: 2,

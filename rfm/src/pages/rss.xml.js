9f4fb3f6068f00c410d37be906fe1f426f7b2e05
pikesley <sam.pikesley@gmail.com>
2023-01-03 21:33:43 +0000

    Generate RSS for `Gigs`

---
+++ rfm/src/pages/rss.xml.js
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss";
+import { fetchFromAPI } from "../tools";
+
+export const get = async () =>
+  rss({
+    title: "Raw Funk Maharishi Gig Calendar",
+    description: "Upcoming Raw Funk Maharishi shows",
+    site: import.meta.env.SITE,
+    stylesheet: "/styles.xsl",
+
+    items: await fetchFromAPI("gigs")
+      .then(function (gigs) {
+        return gigs.filter((gig) => new Date(gig.startDate) > new Date());
+      })
+      .then(function (gigs) {
+        return gigs.map((gig) => {
+          return {
+            link: import.meta.env.SITE.slice(-1) + gig.url, // remove double-slash
+            pubDate: gig.startDate.slice(0, 10),
+            title: gig.location.name,
+          };
+        });
+      }),
+  });

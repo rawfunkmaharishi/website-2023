9aeb6ac14e6fbcbd24edd6ce676f9924bbae7ec5
pikesley <sam.pikesley@gmail.com>
2023-01-04 22:22:01 +0000

    Remove `offSchema` requirement
    And provide better layouts

--- rfm/src/tools.js
+++ rfm/src/tools.js
@@ -5,3 +5,46 @@ export async function fetchFromAPI(page) {
     }
   );
 }
+
+export function streamingServices(referenceUrls) {
+  const urls = [...referenceUrls];
+  const lookups = {
+    "apple.com": {
+      name: "Apple Music",
+      icon: "mdi-apple",
+    },
+    "amazon.co": {
+      name: "Amazon Music",
+      icon: "mdi-music",
+    },
+  };
+
+  // remove ourselves
+  const RFMURL = urls.filter((url) => url.includes("rawfunkmaharishi.uk"))[0];
+  urls.splice(urls.indexOf(RFMURL), 1);
+
+  const YouTubeURL = urls.filter((url) => url.includes("youtube.com"))[0];
+  urls.splice(urls.indexOf(YouTubeURL), 1);
+
+  const SpotifyURL = urls.filter((url) => url.includes("spotify.com"))[0];
+  urls.splice(urls.indexOf(SpotifyURL), 1);
+
+  const otherServices = function () {
+    const results = [];
+    Object.keys(lookups).forEach(function (key) {
+      urls.forEach(function (url) {
+        if (url.includes(key)) {
+          lookups[key]["url"] = url;
+          results.push(lookups[key]);
+        }
+      });
+    });
+    return results;
+  };
+
+  return {
+    YouTubeURL: YouTubeURL,
+    SpotifyURL: SpotifyURL,
+    otherServices: otherServices(),
+  };
+}
---
+++ rfm/tests/tools.test.js
@@ -0,0 +1,31 @@
+import { streamingServices } from "../src/tools.js";
+
+it("structures the data", () => {
+  const sameAs = [
+    "https://rawfunkmaharishi.uk//records/takatsuka",
+    "https://www.youtube.com/embed/videoseries?list=PL_-_2KA93U6SIE2rJJbUlIwRhpns9G_rg",
+    "https://open.spotify.com/embed/album/5GoS0MKtTna4X90fStHjvh",
+    "https://music.apple.com/us/album/takatsuka/1089162061",
+    "https://www.amazon.co.uk/gp/product/B01CH4A5ZG",
+  ];
+
+  const expected = {
+    YouTubeURL:
+      "https://www.youtube.com/embed/videoseries?list=PL_-_2KA93U6SIE2rJJbUlIwRhpns9G_rg",
+    SpotifyURL: "https://open.spotify.com/embed/album/5GoS0MKtTna4X90fStHjvh",
+    otherServices: [
+      {
+        name: "Apple Music",
+        icon: "mdi-apple",
+        url: "https://music.apple.com/us/album/takatsuka/1089162061",
+      },
+      {
+        name: "Amazon Music",
+        icon: "mdi-music",
+        url: "https://www.amazon.co.uk/gp/product/B01CH4A5ZG",
+      },
+    ],
+  };
+
+  expect(streamingServices(sameAs)).toEqual(expected);
+});

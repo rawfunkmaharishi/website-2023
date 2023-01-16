239be1151399abcfd307cc0c6c372080366209df
pikesley <sam.pikesley@gmail.com>
2023-01-16 22:12:47 +0000

    Update tests for new index pages

--- rfm/e2e/titles.spec.ts
+++ rfm/e2e/titles.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from "@playwright/test";
 const expectations = [
   { url: "/", title: "Raw Funk Maharishi" },
   { url: "/the-band", title: "The Band" },
-  { url: "/gigs/1", title: "Upcoming Gigs" },
+  { url: "/gigs", title: "Upcoming Gigs" },
   { url: "/gigs/2023/06/08/luna", title: "Live at Luna Lounge" },
   { url: "/records", title: "Records" },
   { url: "/records/re:designed", title: "Re:Designed" },
-  { url: "/videos/1", title: "Videos" },
+  { url: "/videos", title: "Videos" },
 ];
 
 expectations.forEach(function (expectation) {

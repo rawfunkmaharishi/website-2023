16c0197abd09eddf5509597ca30f6e21b34b57c8
pikesley <sam.pikesley@gmail.com>
2023-01-20 22:43:29 +0000

    Test `nav`

---
+++ rfm/e2e/nav.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+const expectations = [
+  { page: "Band", url: /the-band/ },
+  { page: "Gigs", url: /gigs/ },
+  { page: "Records", url: /records/ },
+  { page: "Videos", url: /videos/ },
+];
+
+expectations.forEach(function (expectation) {
+  test("`" + expectation.page + "` link", async ({ page }) => {
+    await page.goto("/");
+    await page.getByText(expectation.page).click();
+    await expect(page).toHaveURL(expectation.url);
+    await expect(
+      page.getByRole("link", { name: expectation.page })
+    ).toHaveClass(/active/);
+  });
+});

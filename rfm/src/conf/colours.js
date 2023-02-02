dbd2f9859c9f52ba358062b7bab8e82c7ed9de22
pikesley <sam.pikesley@gmail.com>
2023-02-02 22:46:57 +0000

    Nuke logging

--- rfm/src/conf/colours.js
+++ rfm/src/conf/colours.js
@@ -1,7 +1,5 @@
 import { palette, choices } from "./pallets/ffbf00-e83f6f-2274a5-32936f-ffffff";
 
-console.log(palette);
-
 // https://dev.to/devtronic/javascript-map-an-array-of-objects-to-a-dictionary-3f42
 export const RGBs = Object.assign(
   {},

e5e736618180ce7035e9c9fcd8465ed6037f0213
pikesley <sam.pikesley@gmail.com>
2023-02-06 22:32:21 +0000

    More pallets

--- rfm/src/conf/colours.js
+++ rfm/src/conf/colours.js
@@ -1,4 +1,4 @@
-import { palette, choices } from "./pallets/ef6461-e4b363-e8e9eb-e0dfd5-313638";
+import { palette, choices } from "./pallets/820263-d90368-eadeda-2e294e-ffd400";
 
 // https://dev.to/devtronic/javascript-map-an-array-of-objects-to-a-dictionary-3f42
 export const RGBs = Object.assign(
@@ -9,3 +9,5 @@ export const RGBs = Object.assign(
     };
   })
 );
+
+RGBs.background = [255, 255, 255];

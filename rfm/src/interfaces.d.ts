8a74393f5fbd2b64643949a1118f0a329701cbdd
pikesley <sam.pikesley@gmail.com>
2023-02-10 21:57:13 +0000

    Be Typescripty

---
+++ rfm/src/interfaces.d.ts
@@ -0,0 +1,46 @@
+interface Image {
+  contentUrl: string;
+}
+
+interface SlideItem {
+  "@type": string;
+  additionalTitle?: string;
+}
+
+interface Musician extends SlideItem {
+  url: string;
+  name: string;
+  knowsAbout: string;
+  image: {
+    contentUrl: string;
+  };
+}
+
+interface RecordAlbum extends SlideItem {
+  url: string;
+  name: string;
+  datePublished: string;
+  image: Image;
+}
+
+interface EventSet extends SlideItem {
+  items: Event[];
+}
+
+interface GenericSlideItem {
+  [key: any];
+}
+
+interface SingleEvent extends SlideItem {
+  subType: string;
+  item: {
+    recordedIn: {
+      thumbnailUrl: string;
+    };
+    startDate: string;
+    location: {
+      image: Image;
+      name: string;
+    };
+  };
+}

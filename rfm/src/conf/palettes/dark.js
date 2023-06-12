8b88909d6f26555b725229cb5ad86a7dc7a19388
pikesley <sam.pikesley@gmail.com>
2023-06-12 22:43:07 +0100

    Use new palettes

---
+++ rfm/src/conf/palettes/dark.js
@@ -0,0 +1,28 @@
+export const data = {
+  palette: [
+    {
+      name: "Dark grey",
+      rgb: [59, 59, 59],
+    },
+    {
+      name: "Off-white",
+      rgb: [238, 238, 228],
+    },
+    {
+      name: "Teal",
+      rgb: [113, 181, 146],
+    },
+    {
+      name: "Light blue",
+      rgb: [148, 195, 208],
+    },
+  ],
+
+  choices: {
+    primary: 1,
+    link: 2,
+    hover: 3,
+    border: 3,
+    background: 0
+  },
+};

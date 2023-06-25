ea573767866625264523d9d3070638fb9274a664
pikesley <sam.pikesley@gmail.com>
2023-06-25 14:36:45 +0100

    Move to live URL

--- rfm/astro.config.mjs
+++ rfm/astro.config.mjs
@@ -3,8 +3,8 @@ import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://rawfunkmaharishi.uk",
-  site: "https://rawfunkmaharishi.netlify.app", // TODO REMOVE THIS BEFORE RELEASE!!!
+  site: "https://rawfunkmaharishi.uk",
+  // site: "https://rawfunkmaharishi.netlify.app", // TODO REMOVE THIS BEFORE RELEASE!!!
   integrations: [
     htmlBeautifier({
       indent_size: 2,
